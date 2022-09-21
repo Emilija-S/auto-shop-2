abef83a16c9fd1aa33408bd2b2d5ccf8bfce91d7
Emilija-S <emilija.simonoska@hotmail.com>
2022-09-21 19:53:54 +0200

    Defined app model fields

---
+++ database/migrations/2022_09_21_142953_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vehicle_model_id')->constrained();
+            $table->string('chassis_number')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
